23204796d0b6dbdeb0de6315fa229cf84a163deb
charroux <benoit.charroux@gmail.com>
2024-09-10 11:11:39 +0200

    first commit

---
+++ src/main/java/com/charroux/javaCodingRules/JavaCodingRulesApplication.java
@@ -0,0 +1,13 @@
+package com.charroux.javaCodingRules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingRulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charroux/javaCodingRules/JavaCodingRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charroux.javaCodingRules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingRulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
